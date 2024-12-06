517dc7886f6dcfc80505b359f67979c03c38288b
Rohit Bharat Bhandwalkar <rohitb.bhandwalkar@gmail.com>
2024-12-07 03:31:08 +0530

    Added error handling on home page fetch data middleware

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,28 +3,31 @@ import Committee from "../models/committee_model.js";
 
 export const getMemberData = async (req, res, next) => {
 
-    const email = req.query.email;
-    console.log(email);
+    try{
+        const email = req.query.email;
 
-    const memberData = await Member.findOne({ emailId : email });
+        const memberData = await Member.findOne({ emailId : email });
 
-    const committeesAll = await Committee.find().populate({path : 'authorities.memberId', select : 'name emailId'}).lean();
+        const committeesAll = await Committee.find().populate({path : 'authorities.memberId', select : 'name emailId'}).lean();
 
-    const filteredCommittees = committeesAll.map(committee => {
-        return {
-            ...committee,
-            authorities : committee.authorities.filter(auth => auth.position == "Head" || auth.position == "Co-Head").map(auth => ({
-                name : auth.memberId?.name,
-                email : auth.memberId?.emailId,
-                position : auth.position
-            }))
-        }
-    });
+        const filteredCommittees = committeesAll.map(committee => {
+            return {
+                ...committee,
+                authorities : committee.authorities.filter(auth => auth.position == "Head" || auth.position == "Co-Head").map(auth => ({
+                    name : auth.memberId?.name,
+                    email : auth.memberId?.emailId,
+                    position : auth.position
+                }))
+            }
+        });
 
-    console.log(filteredCommittees[0].authorities);
+        console.log(filteredCommittees[0].authorities);
 
-    return res.status(200).json({
-        member : memberData,
-        committees : filteredCommittees
-    });
+        return res.status(200).json({
+            member : memberData,
+            committees : filteredCommittees
+        });
+    } catch (err) {
+        return res.status(500).json({message : "Some internal error occurred please retry after some time"});
+    }
 }
